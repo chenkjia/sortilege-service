b951d5f1fd22688b6eaabb78d4f7a878bb0c3fd7
chenkjia <chenkjia@live.com>
2024-10-24 10:45:10 +0800

    commit

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,8 @@ import * as fs from 'fs';
 
 async function bootstrap() {
   const httpsOptions = {
-    key: fs.readFileSync('../cert/key.pem'),
-    cert: fs.readFileSync('../cert/cert.pem'),
+    key: fs.readFileSync('cert/key.pem'),
+    cert: fs.readFileSync('cert/cert.pem'),
   };
   const app = await NestFactory.create(AppModule, { httpsOptions });
   // const app = await NestFactory.create(AppModule);
